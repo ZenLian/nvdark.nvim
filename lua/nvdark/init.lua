82f4931a327c5fd40e7a36c036e1b162bc1f3582
Zenlian <zeninglian@gmail.com>
2022-05-25 06:27:02 +0800

    stylua format

--- lua/nvdark/init.lua
+++ lua/nvdark/init.lua
@@ -1,22 +1,22 @@
 local M = {}
 
 M.setup = function(options)
-    require('nvdark.config').update(options)
+  require('nvdark.config').update(options)
 end
 
 -- apply colorscheme
 M.load = function()
-    vim.cmd [[highlight clear]]
-    if vim.fn.exists("syntax_on") then
-        vim.cmd("syntax reset")
-    end
-    vim.o.background = "dark"
-    vim.o.termguicolors = true
-    vim.g.colors_name = "nvdark"
+  vim.cmd([[highlight clear]])
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+  vim.o.background = 'dark'
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'nvdark'
 
-    local util = require('nvdark.util')
-    local theme = require('nvdark.theme')
-    util.load(theme)
+  local util = require('nvdark.util')
+  local theme = require('nvdark.theme')
+  util.load(theme)
 end
 
 return M
--- lua/nvdark/style.lua
+++ lua/nvdark/style.lua
@@ -2,11 +2,11 @@ local M = {}
 local config = require('nvdark.config').options
 
 for group, enable in pairs(config.italic) do
-    if config.italic.enable and enable then
-        M[group] = 'italic'
-    else
-        M[group] = 'NONE'
-    end
+  if config.italic.enable and enable then
+    M[group] = 'italic'
+  else
+    M[group] = 'NONE'
+  end
 end
 
 return M
